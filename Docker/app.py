572806f1df732355f059788421cfec3f44e1bf25
ANRame0003 <156145582+ANRame0003@users.noreply.github.com>
2024-01-09 23:44:23 -0800

    Update app.py

--- Docker/app.py
+++ Docker/app.py
@@ -1,15 +1,18 @@
 from utilities.scoreutils import _get_bow, _get_scores
-from flask import Flask, jsonify, Response, request
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="")
+    return 
 
-@app.route("/similarity-scores", methods = ['POST'])
-def compare_sents(sentences):
-  sentences = request.form.get("sentences")
+@app.post("/similarity-scores")
+def compare_sents():
+  request_data = request.get_json()
+  sentences = request_data.get("sentences")
+  stw = request_data.get("stopwords")
+  abbs = request_data.get("abbreviations")
   try:
     if len(sentences) < 2:
         return Response("Please provide 2 sentences to continue", 400)
@@ -18,10 +21,10 @@ def compare_sents(sentences):
     bow = _get_bow(sentences, stw = False, abbs = False) 
     # Separate fn for scoring to change mechanism later
     score = _get_scores(bow) 
-    return round(score, 2) * 100
+    return Response(f"Score is {round(score, 2) * 100}", 201)
   except Exception as e:
      return Response(f"Failed due to error {e}", 500)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port = 3005)
 
